fd13f8eeddbc40dd79eba8a33132ea5c091f62fc
Dhemy <imdhemy@gmail.com>
2024-04-15 12:51:05 +0200

    Fix links in notes bubbles (#16)
    * adjust heading sizes
    
    * Update 2022-12-22-elements.md
    
    * fix: make links clickable in bubbles
    
    * fix table width
    
    - Fixes #15
    
    * fix fonts
    
    * adjust title size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.html'],
-  theme: {
-    fontFamily: {
-      sans: [
-        '"Noto Sans Display',
-        '"Helvetica Neue',
-        'Helvetica',
-        'Arial',
-        'sans-serif'],
-      serif: [
-        '"Noto Serif"',
-        'Georgia',
-        'Cambria',
-        '"Times New Roman"',
-        'Times',
-        'serif'],
-      mono: [
-        'monospace',
-      ],
+    content: ['./**/*.html'],
+    theme: {
+        fontFamily: {
+            sans: [
+                '"Noto Sans Display"',
+                '"Helvetica Neue"',
+                'Helvetica',
+                'Arial',
+                'sans-serif'],
+            serif: [
+                '"Noto Serif"',
+                'Georgia',
+                'Cambria',
+                '"Times New Roman"',
+                'Times',
+                'serif'],
+            mono: [
+                'monospace',
+            ],
+        },
     },
-  },
-  plugins: [require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/typography')],
 };
