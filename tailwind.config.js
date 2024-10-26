1b82187aaa6bf0664763631e480947024719e348
imdhemy <imdhemy@gmail.com>
2024-10-26 22:05:56 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./**/*.html'],
+    content: [
+        './**/*.html',
+        '!./node_modules/**',
+    ],
     theme: {
         fontFamily: {
             sans: [
