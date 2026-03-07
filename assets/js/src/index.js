a073a72b8079b5647fbdb99063396c7cc17dc9c2
Dhemy <imdhemy@gmail.com>
2026-03-07 22:27:58 +0100

    replace ion icons cdn with npm package

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -1,4 +1,25 @@
+import { addIcons } from 'ionicons';
+import { defineCustomElements } from 'ionicons/loader';
+import {
+  close,
+  logoGithub,
+  logoLinkedin,
+  logoTwitter,
+  logoYoutube,
+  menuOutline,
+} from 'ionicons/icons';
+
 const burgerMenu = require('./burger-menu');
+
+defineCustomElements(window);
+addIcons({
+  close,
+  'logo-github': logoGithub,
+  'logo-linkedin': logoLinkedin,
+  'logo-twitter': logoTwitter,
+  'logo-youtube': logoYoutube,
+  'menu-outline': menuOutline,
+});
 
 (() => {
   burgerMenu.init();
